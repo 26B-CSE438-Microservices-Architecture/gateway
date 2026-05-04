e73db253430e3696ab811851860ff5d20cf89be2
Sude Akıncı <akincisude07@gmail.com>
2026-05-04 04:48:02 +0300

    fix: add Secret property to JWTSettings and fix build error

--- backend/CleanArchitecture/CleanArchitecture.Application/Settings/JWTSettings.cs
+++ backend/CleanArchitecture/CleanArchitecture.Application/Settings/JWTSettings.cs
@@ -1,8 +1,9 @@
-﻿namespace CleanArchitecture.Core.Settings
+namespace CleanArchitecture.Core.Settings
 {
     public class JWTSettings
     {
         public string Key { get; set; }
+        public string Secret { get; set; }
         public string Issuer { get; set; }
         public string Audience { get; set; }
         public double DurationInMinutes { get; set; }
